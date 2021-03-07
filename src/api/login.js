b0e7d414a6e8aba0580e359a1554f6e3d970467d
huchongyuan <876510605@qq.com>
2021-03-08 00:15:59 +0800

    新增文件

---
+++ src/api/login.js
@@ -0,0 +1,8 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: huchongyuan
+ * @Date: 2021-03-08 00:15:42
+ * @LastEditors: huchongyuan
+ * @LastEditTime: 2021-03-08 00:15:43
+ */
---
+++ src/utils/request.js
@@ -0,0 +1,8 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: huchongyuan
+ * @Date: 2021-03-08 00:15:18
+ * @LastEditors: huchongyuan
+ * @LastEditTime: 2021-03-08 00:15:19
+ */
